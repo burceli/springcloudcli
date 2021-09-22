fc7e0536539b84a94ff82de418471d1fa033d894
bruceli <xiaoning019@163.com>
2021-09-22 18:36:56 +0800

    动态路由

---
+++ cli_security_8053/src/main/java/me/bruceli/security/CliSecurity8053Application.java
@@ -0,0 +1,15 @@
+package me.bruceli.security;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CliSecurity8053Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CliSecurity8053Application.class, args);
+    }
+
+}
